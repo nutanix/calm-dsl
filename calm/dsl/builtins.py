c6500157dc9f21d254a517fc3f088ab760069e2c
baljanak <balaji@nutanix.com>
2019-02-01 22:48:05 +0530

    do not print resolved dict

--- calm/dsl/builtins.py
+++ calm/dsl/builtins.py
@@ -26,7 +26,7 @@ template = Environment(loader=PackageLoader(__name__, 'schemas')).get_template('
 tdict = yaml.safe_load(StringIO(template.render()))
 
 # Check if all references are resolved
-tdict = jsonref.loads(json.dumps(tdict))
+# tdict = jsonref.loads(json.dumps(tdict))
 
 print(json.dumps(tdict, cls=EntityJSONEncoder, indent=4, separators=(",", ": ")))
 
