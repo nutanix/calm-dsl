c0c3cb5a7ab5f5efb036a408bb1017f145b029c6
Aaditya Sood <aaditya@nutanix.com>
2019-02-22 11:22:06 +0530

    Add RenderJSON as a tool function

---
+++ calm/dsl/tools/__init__.py
@@ -0,0 +1,26 @@
+import uuid
+
+try:
+    from IPython.display import display_javascript, display_html, display
+except ImportError as e:
+    print("Could not import Ipython based classes")
+
+import json
+
+
+class RenderJSON(object):
+    def __init__(self, json_data):
+        if isinstance(json_data, dict):
+            self.json_str = json.dumps(json_data)
+        else:
+            self.json_str = json_data
+        self.uuid = str(uuid.uuid4())
+
+    def _ipython_display_(self):
+        display_html(
+            '<div id="{}" style="height: 600px; width:100%;"></div>'.format(self.uuid), raw=True)
+        display_javascript("""
+        require(["https://rawgit.com/caldwell/renderjson/master/renderjson.js"], function() {
+        document.getElementById('%s').appendChild(renderjson(%s))
+        });
+        """ % (self.uuid, self.json_str), raw=True)
