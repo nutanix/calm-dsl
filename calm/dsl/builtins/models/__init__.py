aca0d14849b9a7175d3e4e0a6bae27c0c8a9311c
baljanak <balaji@nutanix.com>
2019-02-28 22:54:26 +0530

    Add package to the mix

--- calm/dsl/builtins/models/__init__.py
+++ calm/dsl/builtins/models/__init__.py
@@ -2,6 +2,7 @@ from .ref import Ref, ref
 from .variable import Variable, var, setvar
 from .port import Port, port, port_type
 from .service import Service, service, service_type
+from .package import Package, package, package_type
 from .substrate import Substrate, substrate, substrate_type
 from .deployment import Deployment, deployment, deployment_type
 from .profile import Profile, profile, profile_type
