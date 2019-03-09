d7821b40c5034d98584935bcf1d74af3d6b5e664
baljanak <balaji@nutanix.com>
2019-03-09 09:35:58 +0530

    remove #noqa tag

--- calm/dsl/builtins/__init__.py
+++ calm/dsl/builtins/__init__.py
@@ -1,1 +1,24 @@
-from .models import *  # noqa: F401 F403
+from .models.ref import Ref, ref
+from .models.credential import basic_cred
+from .models.variable import Variable, var, setvar
+from .models.port import Port, port
+from .models.service import Service, service
+from .models.package import Package, package
+from .models.substrate import Substrate, substrate
+from .models.deployment import Deployment, deployment
+from .models.profile import Profile, profile
+from .models.blueprint import Blueprint, blueprint
+
+
+__all__ = [
+    'Ref', 'ref',
+    'basic_cred',
+    'Variable', 'var', 'setvar',
+    'Port', 'port',
+    'Service', 'service',
+    'Package', 'package',
+    'Substrate', 'substrate',
+    'Deployment', 'deployment',
+    'Profile', 'profile',
+    'Blueprint', 'blueprint',
+]
