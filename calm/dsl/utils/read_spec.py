b84eaedf9d3ba063225f2d7cad771723fc4620d4
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-05-15 16:21:00 +0530

    Added spec validator AHV Modified create_spec properties in substrate Modified test_single_vm_bp.py file

--- calm/dsl/utils/read_spec.py
+++ calm/dsl/utils/read_spec.py
@@ -12,3 +12,53 @@ def read_ahv_vm_spec(filename):
 
     with open(file_path, "r") as f:
         return yaml.safe_load(f.read())
+
+
+class CreateSpecReader:
+
+    def __init__(self, create_spec):
+
+        self.create_spec = create_spec
+    
+    def __validate__(self, vm_type):
+
+        Providers = {
+            'AHV_VM': 
+            ('calm.dsl.builtins.models.create_spec_validator', 'AHV_Validator')
+        }
+
+        if vm_type not in Providers:
+            raise Exception('given provider : %r is not supported' % (vm_type) )
+        
+        mod_name, validator_name = Providers[vm_type]  
+        validator_mod = __import__(
+            mod_name, globals(), locals(), validator_name
+        )
+        
+        validator_cls = getattr(validator_mod, validator_name)
+        validator = validator_cls()
+        validator.validate(self.create_spec)
+
+        return self.create_spec
+
+    def __get__(self, instance, cls):
+
+        if cls is None:
+            return self
+
+        vm_type = cls.type
+        return self.__validate__(vm_type)
+
+
+def read_vm_spec(filename):
+
+    file_path = os.path.join(
+        os.path.dirname(
+            inspect.getfile(
+                sys._getframe(1))),
+        filename)
+
+    with open(file_path, "r") as f:
+        create_spec = yaml.safe_load(f.read())
+
+    return CreateSpecReader(create_spec)
