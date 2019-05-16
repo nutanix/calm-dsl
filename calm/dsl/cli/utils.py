6a22f890593aae27bf155cc3811291fe06667ab0
Aditya Sen <aditya.sen@nutanix.com>
2019-05-16 13:51:27 +0530

    Break out app cli functions into a separate file

--- calm/dsl/cli/utils.py
+++ calm/dsl/cli/utils.py
@@ -1,3 +1,7 @@
+import click
+from functools import reduce
+
+
 def get_states_filter(STATES_CLASS, state_key="state"):
 
     states = []
@@ -6,3 +10,21 @@ def get_states_filter(STATES_CLASS, state_key="state"):
             states.append(getattr(STATES_CLASS, field))
     state_prefix = ",{}==".format(state_key)
     return ";({}=={})".format(state_key, state_prefix.join(states))
+
+
+def get_name_query(names):
+    if names:
+        search_strings = [
+            "name==.*"
+            + reduce(
+                lambda acc, c: "{}[{}|{}]".format(acc, c.lower(), c.upper()), name, ""
+            )
+            + ".*"
+            for name in names
+        ]
+        return ",".join(search_strings)
+
+
+def highlight_text(text, **kwargs):
+    """Highlight text in our standard format"""
+    return click.style("{}".format(text), fg="blue", bold=False, **kwargs)
