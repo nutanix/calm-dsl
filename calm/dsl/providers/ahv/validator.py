8286b0d407398b6bc896088e3d6eaef4673390df
baljanak <balaji@nutanix.com>
2019-05-22 13:09:41 +0530

    call it provider

--- calm/dsl/providers/ahv/validator.py
+++
@@ -1,8 +0,0 @@
-from ..providers import get_validator_interface
-
-
-ValidatorBase = get_validator_interface()
-
-
-class AhvVmValidator(ValidatorBase, provider_type="AHV_VM", package_name=__name__):
-    pass
