76668ab5fa93a67bcd4030cbc999c6351948c31d
baljanak <balaji@nutanix.com>
2019-05-22 11:16:19 +0530

    fix tests

--- calm/dsl/providers/__init__.py
+++ calm/dsl/providers/__init__.py
@@ -3,4 +3,8 @@ from .existing_machine import EM_Validator
 from .providers import get_validator
 
 
-__all__ = ["get_validator"]
+__all__ = [
+    'get_validator',
+    'AHV_Validator',
+    'EM_Validator',
+]
