14502bc9d9b88f18a5e0733e6281d4fdf810519b
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-05-27 12:16:05 +0530

    Some basic improvements. Removed sleep() from projects

--- calm/dsl/api/projects.py
+++ calm/dsl/api/projects.py
@@ -5,23 +5,14 @@ import time
 class ProjectAPI(ResourceAPI):
 
     CREATE_PREFIX = ResourceAPI.PREFIX + "projects_internal"
-    _PREFIX = ResourceAPI.PREFIX + "projects"
-    _LIST = _PREFIX + "/list"
-    _ITEM = _PREFIX + "/{}"
+    PREFIX = ResourceAPI.PREFIX + "projects"
+    LIST = PREFIX + "/list"
+    ITEM = PREFIX + "/{}"
 
     def create(self, payload):
-        res, err = self.connection._call(
+        return self.connection._call(
             self.CREATE_PREFIX,
             verify=False,
             request_json=payload,
             method=REQUEST.METHOD.POST,
         )
-
-        time.sleep(8)  # Project apis are intentful , wait to get consumed
-        return res, err
-
-    def delete(self, uuid):
-        res, err = super.delete(self, uuid)
-
-        time.sleep(10)  # Wait for Project delete api to get consumed
-        return res, err
