94e21d318ac1d2c2bd37d2e37042732621f31aa9
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-06-03 15:50:39 +0530

    Added delete command for projects

--- calm/dsl/cli/__init__.py
+++ calm/dsl/cli/__init__.py
@@ -19,7 +19,12 @@ from .bps import (
     launch_blueprint_simple,
     delete_blueprint,
 )
+<<<<<<< HEAD
 from .projects import get_projects
+=======
+from .projects import get_projects,delete_project
+from .providers import create_ahv_spec
+>>>>>>> Added delete command for projects
 
 
 @click.group()
@@ -301,6 +306,13 @@ def _delete_blueprint(obj, blueprint_names):
 @click.pass_obj
 def _delete_app(obj, app_names, soft):
     delete_app(obj, app_names, soft)
+
+
+@delete.command("project")
+@click.argument("project_names", nargs=-1)
+@click.pass_obj
+def _delete_project(obj, project_names):
+    delete_project(obj, project_names)
 
 
 @main.group()
