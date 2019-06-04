17a04325176980032844e28c99088b18bee6de8e
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-06-04 13:03:12 +0530

    Added command for creation of project using a json/yaml file

--- calm/dsl/cli/__init__.py
+++ calm/dsl/cli/__init__.py
@@ -19,7 +19,7 @@ from .bps import (
     launch_blueprint_simple,
     delete_blueprint,
 )
-from .projects import get_projects,delete_project
+from .projects import get_projects, delete_project, create_project
 from .providers import create_ahv_spec
 
 
@@ -281,6 +281,40 @@ def create_blueprint_command(obj, bp_file, name, description):
     bp_state = bp["status"]["state"]
     click.echo(">> Blueprint state: {}".format(bp_state))
     assert bp_state == "ACTIVE"
+
+
+def create_project_from_file(client, file_location):
+
+    project_payload = yaml.safe_load(open(file_location, "r").read())
+    return create_project(client, project_payload)
+
+
+@create.command("project")
+@click.option(
+    "--file",
+    "-f",
+    "project_file",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
+    help="Path of Project file to upload",
+)
+@click.pass_obj
+def _create_project(obj, project_file):
+    """ Creates the project """
+
+    client = obj.get("client")
+
+    if project_file.endswith(".json") or project_file.endswith(".json"):
+        res, err = create_project_from_file(client, project_file)
+    else:
+        click.echo("Unknown file format")
+
+    if err:
+        click.echo(err["error"])
+        return
+
+    project = res.json()
+    state = project["status"]["state"]
+    click.echo(">> Project state: {}".format(state))
 
 
 @main.group()
