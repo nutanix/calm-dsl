a25f98353fdde84d2bfcc73a8df7373a66714570
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-06-24 12:44:53 +0530

    format using black

--- calm/dsl/providers/plugins/vmware_vm/constants.py
+++ calm/dsl/providers/plugins/vmware_vm/constants.py
@@ -1,153 +1,128 @@
 class VCENTER:
 
-    VERSION = 'v6'
-    DATACENTER = 'vmware/{}/datacenter'. format(VERSION)
-    TEMPLATE = 'vmware/{}/template'. format(VERSION)
-    DATASTORE = 'vmware/{}/datastore'. format(VERSION)
-    HOST = 'vmware/{}/host'. format(VERSION)
-    CLUSTER = 'vmware/{}/cluster'. format(VERSION)
-    STORAGE_POD = 'vmware/{}/storage_pod'. format(VERSION)
-    NETWORK = 'vmware/{}/network'. format(VERSION)
-    NETWORK_ADAPTER = 'vmware/{}/network_adapter'. format(VERSION)
-    CUSTOMIZATION = 'vmware/{}/customization'. format(VERSION)
-    TIMEZONE = 'vmware/{}/timezone'. format(VERSION)
-    ACCOUNTS = 'vmware/{}/accounts'. format(VERSION)
-    TEMPLATE_DEFS = ('vmware/{}/accounts/'. format(VERSION)) + '{}/templates'
-    FILE_PATHS = 'vmware/{}/file_paths'. format(VERSION)
+    VERSION = "v6"
+    DATACENTER = "vmware/{}/datacenter".format(VERSION)
+    TEMPLATE = "vmware/{}/template".format(VERSION)
+    DATASTORE = "vmware/{}/datastore".format(VERSION)
+    HOST = "vmware/{}/host".format(VERSION)
+    CLUSTER = "vmware/{}/cluster".format(VERSION)
+    STORAGE_POD = "vmware/{}/storage_pod".format(VERSION)
+    NETWORK = "vmware/{}/network".format(VERSION)
+    NETWORK_ADAPTER = "vmware/{}/network_adapter".format(VERSION)
+    CUSTOMIZATION = "vmware/{}/customization".format(VERSION)
+    TIMEZONE = "vmware/{}/timezone".format(VERSION)
+    ACCOUNTS = "vmware/{}/accounts".format(VERSION)
+    TEMPLATE_DEFS = ("vmware/{}/accounts/".format(VERSION)) + "{}/templates"
+    FILE_PATHS = "vmware/{}/file_paths".format(VERSION)
 
     POWER_STATE = {
         "POWER_ON": "poweron",
         "POWER_OFF": "poweroff",
         "ON": "ON",
-        "OFF": "OFF"
+        "OFF": "OFF",
     }
 
-    DISK_ADAPTER_TYPES = {
-        "SCSI": 'SCSI',
-        "IDE": 'IDE',
-        "SATA": 'SATA'
-    }
+    DISK_ADAPTER_TYPES = {"SCSI": "SCSI", "IDE": "IDE", "SATA": "SATA"}
 
-    DISK_TYPES = {
-        "DISK": 'disk',
-        "CD-ROM": 'cdrom',
-    }
+    DISK_TYPES = {"DISK": "disk", "CD-ROM": "cdrom"}
 
-    DISK_ADAPTERS = {
-        "disk": ['SCSI', 'SATA'],
-        "cdrom": ['IDE']
-    }
+    DISK_ADAPTERS = {"disk": ["SCSI", "SATA"], "cdrom": ["IDE"]}
 
     DISK_MODE = {
-        "Independent - Persistent": 'independent_persistent',
-        "Dependent": 'persistent',
-        "Independent - Nonpersistent": 'independent_nonpersistent'
+        "Independent - Persistent": "independent_persistent",
+        "Dependent": "persistent",
+        "Independent - Nonpersistent": "independent_nonpersistent",
     }
 
     CONTROLLER = {
-        'SCSI': {
-            'Lsi Logic Parallel': 'VirtualLsiLogicController',
-            'Lsi Logic SAS': 'VirtualLsiLogicSASController',
-            'VMware Paravirtual': 'ParaVirtualSCSIController',
-            'Bus Logic Parallel': 'VirtualBusLogicController'
+        "SCSI": {
+            "Lsi Logic Parallel": "VirtualLsiLogicController",
+            "Lsi Logic SAS": "VirtualLsiLogicSASController",
+            "VMware Paravirtual": "ParaVirtualSCSIController",
+            "Bus Logic Parallel": "VirtualBusLogicController",
         },
-        'SATA': {
-            'Virtual SATA Controller': 'VirtualAHCIController'
-        }
+        "SATA": {"Virtual SATA Controller": "VirtualAHCIController"},
     }
 
     BUS_SHARING = {
-        'No Sharing': 'noSharing',
-        'Virtual Sharing': 'virtualSharing',
-        'Physical Sharing': 'physicalSharing'
+        "No Sharing": "noSharing",
+        "Virtual Sharing": "virtualSharing",
+        "Physical Sharing": "physicalSharing",
     }
 
     KEY_BASE = {
-        'CONTROLLER': {
-            'SCSI': 1000,
-            'SATA': 15000,
-            'IDE': 200
-        },
-        'NETWORK': 4000,
-        'DISK': 2000
+        "CONTROLLER": {"SCSI": 1000, "SATA": 15000, "IDE": 200},
+        "NETWORK": 4000,
+        "DISK": 2000,
     }
 
-    ControllerLimit = {
-        "SCSI": 4,
-        "SATA": 4,
-        "IDE": 2
-    }
+    ControllerLimit = {"SCSI": 4, "SATA": 4, "IDE": 2}
 
-    OperatingSystem = {
-        "Windows": "GUEST_OS_WINDOWS",
-        "LINUX": "GUEST_OS_LINUX"
-    }
+    OperatingSystem = {"Windows": "GUEST_OS_WINDOWS", "LINUX": "GUEST_OS_LINUX"}
 
     GuestCustomizationModes = {
-        "CloudInit": 'CLOUD_INIT',
-        "CustomSpec": 'CUSTOM_SPEC',
-        "PredefinedCustomization": 'PREDEFINED_CUSTOMIZATION',
+        "CloudInit": "CLOUD_INIT",
+        "CustomSpec": "CUSTOM_SPEC",
+        "PredefinedCustomization": "PREDEFINED_CUSTOMIZATION",
     }
 
     VirtualControllerNameMap = {
-        'vim.vm.device.VirtualIDEController': 'VirtualIDEController',
-        'vim.vm.device.VirtualLsiLogicSASController': 'VirtualLsiLogicSASController',
-        'vim.vm.device.VirtualSCSIController': 'VirtualSCSIController',
-        'vim.vm.device.VirtualSATAController': 'VirtualSATAController',
-        'vim.vm.device.ParaVirtualSCSIController': 'ParaVirtualSCSIController',
-        'vim.vm.device.VirtualAHCIController': 'VirtualAHCIController',
-        'vim.vm.device.VirtualBusLogicController': 'VirtualBusLogicController',
-        'vim.vm.device.VirtualLsiLogicController': 'VirtualLsiLogicController'
+        "vim.vm.device.VirtualIDEController": "VirtualIDEController",
+        "vim.vm.device.VirtualLsiLogicSASController": "VirtualLsiLogicSASController",
+        "vim.vm.device.VirtualSCSIController": "VirtualSCSIController",
+        "vim.vm.device.VirtualSATAController": "VirtualSATAController",
+        "vim.vm.device.ParaVirtualSCSIController": "ParaVirtualSCSIController",
+        "vim.vm.device.VirtualAHCIController": "VirtualAHCIController",
+        "vim.vm.device.VirtualBusLogicController": "VirtualBusLogicController",
+        "vim.vm.device.VirtualLsiLogicController": "VirtualLsiLogicController",
     }
 
     SCSIControllerOptions = {
-        'VirtualLsiLogicController': 'Lsi Logic Parallel',
-        'VirtualLsiLogicSASController': 'Lsi Logic SAS',
-        'ParaVirtualSCSIController': 'VMware Paravirtual',
-        'VirtualBusLogicController': 'Bus Logic Parallel'
+        "VirtualLsiLogicController": "Lsi Logic Parallel",
+        "VirtualLsiLogicSASController": "Lsi Logic SAS",
+        "ParaVirtualSCSIController": "VMware Paravirtual",
+        "VirtualBusLogicController": "Bus Logic Parallel",
     }
 
-    SATAControllerOptions = {
-        'VirtualAHCIController': 'Virtual SATA Controller'
-    }
+    SATAControllerOptions = {"VirtualAHCIController": "Virtual SATA Controller"}
 
     ControllerMap = {
-        'vim.vm.device.VirtualIDEController': 'IDE',
-        'vim.vm.device.VirtualLsiLogicSASController': 'SCSI',
-        'vim.vm.device.VirtualSCSIController': 'SCSI',
-        'vim.vm.device.VirtualSATAController': 'SATA',
-        'vim.vm.device.ParaVirtualSCSIController': 'SCSI',
-        'vim.vm.device.VirtualAHCIController': 'SATA',
-        'vim.vm.device.VirtualBusLogicController': 'SCSI',
-        'vim.vm.device.VirtualLsiLogicController': 'SCSI'
+        "vim.vm.device.VirtualIDEController": "IDE",
+        "vim.vm.device.VirtualLsiLogicSASController": "SCSI",
+        "vim.vm.device.VirtualSCSIController": "SCSI",
+        "vim.vm.device.VirtualSATAController": "SATA",
+        "vim.vm.device.ParaVirtualSCSIController": "SCSI",
+        "vim.vm.device.VirtualAHCIController": "SATA",
+        "vim.vm.device.VirtualBusLogicController": "SCSI",
+        "vim.vm.device.VirtualLsiLogicController": "SCSI",
     }
 
     ControllerDeviceSlotMap = {
-        'VirtualIDEController': 2,
-        'VirtualLsiLogicSASController': 16,
-        'VirtualSCSIController': 16,
-        'VirtualSATAController': 30,
-        'ParaVirtualSCSIController': 16,
-        'VirtualAHCIController': 30,
-        'VirtualBusLogicController': 16,
-        'VirtualLsiLogicController': 16,
+        "VirtualIDEController": 2,
+        "VirtualLsiLogicSASController": 16,
+        "VirtualSCSIController": 16,
+        "VirtualSATAController": 30,
+        "ParaVirtualSCSIController": 16,
+        "VirtualAHCIController": 30,
+        "VirtualBusLogicController": 16,
+        "VirtualLsiLogicController": 16,
     }
 
     NetworkAdapterMap = {
-        'vim.vm.device.VirtualE1000': 'e1000',
-        'vim.vm.device.VirtualE1000e': 'e1000e',
-        'vim.vm.device.VirtualPCNet32': 'pcnet32',
-        'vim.vm.device.VirtualVmxnet': 'vmxnet',
-        'vim.vm.device.VirtualVmxnet2': 'vmxnet2',
-        'vim.vm.device.VirtualVmxnet3': 'vmxnet3',
+        "vim.vm.device.VirtualE1000": "e1000",
+        "vim.vm.device.VirtualE1000e": "e1000e",
+        "vim.vm.device.VirtualPCNet32": "pcnet32",
+        "vim.vm.device.VirtualVmxnet": "vmxnet",
+        "vim.vm.device.VirtualVmxnet2": "vmxnet2",
+        "vim.vm.device.VirtualVmxnet3": "vmxnet3",
     }
 
     templateDiskEditables = {
-        "Disk Size in GB": 'disk_size_mb',
-        "Disk Mode ": 'disk_mode'
+        "Disk Size in GB": "disk_size_mb",
+        "Disk Mode ": "disk_mode",
     }
 
     DiskMap = {
-        'vim.vm.device.VirtualDisk': 'disk',
-        'vim.vm.device.VirtualCdrom': 'cdrom',
+        "vim.vm.device.VirtualDisk": "disk",
+        "vim.vm.device.VirtualCdrom": "cdrom",
     }
