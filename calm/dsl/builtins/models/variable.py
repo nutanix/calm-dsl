07ea3ecbb4c95c2d59440f741d702db25ff2fa48
Kiran Shenoy <kiran.shenoy@nutanix.com>
2019-06-25 12:18:17 +0530

    Fix variable schema and helpers

--- calm/dsl/builtins/models/variable.py
+++ calm/dsl/builtins/models/variable.py
@@ -52,11 +52,12 @@ Variable = _var()
 
 def setvar(name, value, type_="LOCAL", **kwargs):
 
+    if name is None:
+        name = getattr(VariableType, "__schema_name__")
     kwargs["name"] = name
     kwargs["value"] = value
     kwargs["type"] = type_
 
-    # name = name.title() + getattr(VariableType, "__schema_name__")
     return VariableType(name, (Variable,), kwargs)
 
 
@@ -172,7 +173,7 @@ def _advanced_variable(
         if data_type not in VARIABLE_DATA_TYPES.values():
             raise ValueError("Invalid data_type provided for variable " + (name or ""))
         kwargs["data_type"] = data_type
-    if regex is not None:
+    if task is not None:
         if not getattr(task, "__kind__") == "app_task":
             raise TypeError(
                 "Expected string in field regex for variable "
