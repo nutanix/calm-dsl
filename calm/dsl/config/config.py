020dcf6cb8f0ac34af29edf6a439d307b3d42d48
kumarsarath588 <kumar.sarath588@gmail.com>
2019-08-22 14:18:11 +0530

    changed back config

--- calm/dsl/config/config.py
+++ calm/dsl/config/config.py
@@ -4,8 +4,7 @@ import configparser
 
 # Defaults to be used if no config file exists.
 # TODO - remove username/password
-#PC_IP = "10.46.34.230"
-PC_IP = "10.46.4.2"
+PC_IP = "10.46.34.230"
 PC_PORT = 9440
 PC_USERNAME = "admin"
 PC_PASSWORD = "***REMOVED***"
