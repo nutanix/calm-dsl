f424650a11114629aff0b0f73071a2d74618c7ed
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-08-28 11:36:51 +0530

    Minor changes in k8s_pod_dependencies

--- calm/dsl/builtins/models/blueprint.py
+++ calm/dsl/builtins/models/blueprint.py
@@ -19,118 +19,125 @@ class BlueprintType(EntityType):
 
     def compile(cls):
 
-        cdict = super().compile()
+        def pod_deployment_creation(cdict):
+            """ Implement pod deployment if exists """
+            service_definition_list = []
+            package_definition_list = []
+            substrate_definition_list = []
+            published_service_definition_list = []
 
-        service_definition_list = []
-        package_definition_list = []
-        substrate_definition_list = []
-        published_service_definition_list = []
+            for profile in cdict["app_profile_list"]:
+                pod_deployments = getattr(profile, "pod_deployments", [])
+                if pod_deployments:
+                    delattr(profile, "pod_deployments")
 
-        for profile in cdict["app_profile_list"]:
-            pod_deployments = []
-            pod_deployments = profile.pod_deployments
-            delattr(profile, "pod_deployments")
+                container_service_map = {}
 
-            container_service_map = {}
+                for dep in pod_deployments:
+                    pub_service_name = dep.__name__ + "Published_Service"
+                    ps_options = {"type": "PROVISION_K8S_SERVICE"}
+                    ps_options = {**ps_options, **dep.service_spec}
 
-            for dep in pod_deployments:
-                service_name = dep.__name__ + "Published_Service"
-                ps_options = {"type": "PROVISION_K8S_SERVICE"}
-                ps_options = {**ps_options, **dep.service_spec}
+                    ps = published_service(name=pub_service_name, options=ps_options)
+                    published_service_definition_list.append(ps)
 
-                ps = published_service(name=service_name, options=ps_options)
-                published_service_definition_list.append(ps)
+                    containers_list = dep.deployment_spec["spec"]["template"]["spec"].pop("containers", None)
 
-                containers_list = dep.deployment_spec["spec"]["template"]["spec"].pop("containers", None)
+                    package_references = []
+                    for container in containers_list:
+                        img = container.pop("image", "")
+                        img_pull_policy = container.pop("imagePullPolicy", None)
 
-                package_references = []
-                for container in containers_list:
-                    img = container.pop("image", "")
-                    img_pull_policy = container.pop("imagePullPolicy", None)
+                        container_name = container["name"]
 
-                    container_name = container["name"]
+                        s = service(
+                            name=container_name + str(uuid.uuid4())[-10:] + "Service",
+                            container_spec=container
+                        )
+                        container_service_map[dep.__name__ + "." + container_name] = s
 
-                    s = service(
-                        name=container_name + str(uuid.uuid4())[-10:] + "Service",
-                        container_spec=container
-                    )
-                    container_service_map[dep.__name__ + "." + container_name] = s
+                        if img_pull_policy:
+                            image_spec = {
+                                "image": img,
+                                "imagePullPolicy": img_pull_policy
+                            }
 
-                    if img_pull_policy:
-                        image_spec = {
-                            "image": img,
-                            "imagePullPolicy": img_pull_policy
-                        }
-
-                    else:
-                        image_spec = {"image": img}
-
-                    p = package(
-                        name=container_name + str(uuid.uuid4())[-10:] + "Package",
-                        image_spec=image_spec,
-                        type="K8S_IMAGE"
-                    )
-                    p.services = [ref(s)]
-                    package_references.append(ref(p))
-
-                    # Storing services and packages to serivce list
-                    service_definition_list.append(s)
-                    package_definition_list.append(p)
-
-                sub_provider_spec = dep.deployment_spec["spec"].pop("template", {})
-                sub_provider_spec = {**({"type": "PROVISION_K8S_POD"}), **sub_provider_spec}
-                sub = substrate(
-                    name=container_name + str(uuid.uuid4())[-10:] + "Substrate",
-                    provider_type="K8S_POD",
-                    provider_spec=sub_provider_spec
-                )
-
-                substrate_definition_list.append(sub)
-
-                dep_options = {"type": "PROVISION_K8S_DEPLOYMENT"}
-                dep_options = {**dep_options, **(dep.deployment_spec)}
-                d = deployment(
-                    name=dep.__name__,
-                    options=dep_options,
-                    type="K8S_DEPLOYMENT",
-                    max_replicas="100"
-                )
-
-                d.published_services = [ref(ps)]
-                d.packages = package_references
-                d.substrate = ref(sub)
-
-                profile.deployments.append(d)
-
-            for dep in pod_deployments:
-                dependencies = getattr(dep, "dependencies")
-                for key, entities in dependencies.items():
-                    if isinstance(key, str):
-                        if key.find(".") == -1:
-                            key = dep.__name__ + "." + key  # Key for finding service class
-
-                        serv1 = container_service_map[key]
-                    else:
-                        serv1 = key     # If it is a class(Service like PHPService)
-
-                    for entity in entities:
-                        if isinstance(entity, str):
-                            if entity.find(".") == -1:
-                                entity = dep.__name__ + "." + entity    # Key for finding service class
-
-                            serv2 = container_service_map[entity]
                         else:
-                            serv2 = entity      # If it is a class(Service like PHPService)
+                            image_spec = {"image": img}
 
-                        cur_depends = serv1.dependencies
-                        cur_depends.append(ref(serv2))
-                        setattr(serv1, "dependencies", cur_depends)
+                        p = package(
+                            name=container_name + str(uuid.uuid4())[-10:] + "Package",
+                            image_spec=image_spec,
+                            type="K8S_IMAGE"
+                        )
+                        p.services = [ref(s)]
+                        package_references.append(ref(p))
 
-        cdict["service_definition_list"].extend(service_definition_list)
-        cdict["package_definition_list"].extend(package_definition_list)
-        cdict["substrate_definition_list"].extend(substrate_definition_list)
-        cdict["published_service_definition_list"].extend(published_service_definition_list)
+                        # Storing services and packages to serivce list
+                        service_definition_list.append(s)
+                        package_definition_list.append(p)
 
+                    sub_provider_spec = dep.deployment_spec["spec"].pop("template", {})
+                    sub_provider_spec = {**({"type": "PROVISION_K8S_POD"}), **sub_provider_spec}
+                    sub = substrate(
+                        name=container_name + str(uuid.uuid4())[-10:] + "Substrate",
+                        provider_type="K8S_POD",
+                        provider_spec=sub_provider_spec
+                    )
+
+                    substrate_definition_list.append(sub)
+
+                    dep_options = {"type": "PROVISION_K8S_DEPLOYMENT"}
+                    dep_options = {**dep_options, **(dep.deployment_spec)}
+                    d = deployment(
+                        name=dep.__name__,
+                        options=dep_options,
+                        type="K8S_DEPLOYMENT",
+                        max_replicas="100"
+                    )
+
+                    d.published_services = [ref(ps)]
+                    d.packages = package_references
+                    d.substrate = ref(sub)
+                    d.dependencies = getattr(dep, "dependencies")
+
+                    profile.deployments.append(d)
+
+                """     Container-level dependencies
+                for dep in pod_deployments:
+                    dependencies = getattr(dep, "dependencies")
+                    for key, entities in dependencies.items():
+                        if isinstance(key, str):
+                            if key.find(".") == -1:
+                                key = dep.__name__ + "." + key  # Key for finding service class
+
+                            serv1 = container_service_map[key]
+                        else:
+                            serv1 = key     # If it is a class(Service like PHPService)
+
+                        for entity in entities:
+                            if isinstance(entity, str):
+                                if entity.find(".") == -1:
+                                    entity = dep.__name__ + "." + entity    # Key for finding service class
+
+                                serv2 = container_service_map[entity]
+                            else:
+                                serv2 = entity      # If it is a class(Service like PHPService)
+
+                            cur_depends = serv1.dependencies
+                            cur_depends.append(ref(serv2))
+                            setattr(serv1, "dependencies", cur_depends)
+                """
+
+            cdict["service_definition_list"].extend(service_definition_list)
+            cdict["package_definition_list"].extend(package_definition_list)
+            cdict["substrate_definition_list"].extend(substrate_definition_list)
+            cdict["published_service_definition_list"].extend(published_service_definition_list)
+
+            return cdict
+
+        cdict = super().compile()
+        cdict = pod_deployment_creation(cdict)
         return cdict
 
 
