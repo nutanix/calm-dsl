a09ad9ab22610909ee3e68b1d862e2be908acdc4
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-09-13 16:12:50 +0530

    Added some more fields for the secrets and better view for listing the secrets

--- calm/dsl/cli/secrets.py
+++ calm/dsl/cli/secrets.py
@@ -1,4 +1,5 @@
 import click
+from prettytable import PrettyTable
 
 from .utils import highlight_text
 from calm.dsl.builtins import _create_secret, _delete_secret, _update_secret, list_secrets, _find_secret
@@ -7,7 +8,7 @@ from calm.dsl.builtins import _create_secret, _delete_secret, _update_secret, li
 def create_secret(name, value, pass_phrase=""):
     """Creates the secret"""
 
-    secrets = list_secrets()
+    secrets = get_secrets_names()
     if name in secrets:
         click.echo(highlight_text("Secret Already present !!!\nTry to update secret\n"))
         return
@@ -16,18 +17,41 @@ def create_secret(name, value, pass_phrase=""):
     click.echo(highlight_text("\nSecret created !!! \n"))
 
 
-def get_secrets():
+def get_secrets(quiet):
     """List the secrets"""
 
     avl_secrets = list_secrets()
+
+    if quiet:
+        for secret in avl_secrets:
+            click.echo(highlight_text(secret.name))
+        return
+
+    table = PrettyTable()
+    table.field_names = [
+        "NAME",
+        "CREATED ON",
+        "LAST UPDATED",
+        "UUID",
+    ]
+
     for secret in avl_secrets:
-        click.echo(highlight_text(secret))
+        table.add_row(
+            [
+                highlight_text(secret.name),
+                highlight_text(secret.creation_time),
+                highlight_text(secret.last_update_time),
+                highlight_text(secret.uuid)
+            ]
+        )
+
+    click.echo(table)
 
 
 def delete_secret(name):
     """Deletes the secret"""
 
-    secrets = list_secrets()
+    secrets = get_secrets_names()
     if name not in secrets:
         click.echo(highlight_text("\nSecret not present !!!\n"))
         return
@@ -39,7 +63,7 @@ def delete_secret(name):
 def update_secret(name, value, pass_phrase):
     """Updates the secret"""
 
-    secrets = list_secrets()
+    secrets = get_secrets_names()
     if name not in secrets:
         click.echo(highlight_text("\nSecret not present !!!\n"))
         return
@@ -48,8 +72,18 @@ def update_secret(name, value, pass_phrase):
     click.echo(highlight_text("\nSecret updated !!!\n"))
 
 
-def find_secret(name, pass_phrase):
+def find_secret(name, pass_phrase=""):
     """ Gives you the value stored correponding to secret"""
 
     secret_val = _find_secret(name, pass_phrase)
     return secret_val
+
+
+def get_secrets_names():
+
+    secrets = list_secrets()
+    secret_names = []
+    for secret in secrets():
+        secret_names.append(secret.name)
+
+    return secret_names
