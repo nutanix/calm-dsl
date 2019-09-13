4eea6a168092b79aaaddc6a76c86d800afe4bd80
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-09-13 14:34:31 +0530

    Added `pycryptodome` encruption/decryption on secrets

--- calm/db/helper.py
+++ calm/db/helper.py
@@ -1,5 +1,7 @@
-from peewee import SqliteDatabase, Model, CharField
+from peewee import SqliteDatabase, Model, CharField, BlobField
 import os
+
+from .crypto import encrypt_AES_GCM, decrypt_AES_GCM
 
 
 db_location = "calm/db/dsl.db"
@@ -11,7 +13,14 @@ db.connect()
 
 class Secret(Model):
     name = CharField()
-    value = CharField()
+    kdf_salt = BlobField()
+    ciphertext = BlobField()
+    iv = BlobField()
+    auth_tag = BlobField()
+    pass_phrase = BlobField()
+
+    def generate_enc_msg(self):
+        return (self.kdf_salt, self.ciphertext, self.iv, self.auth_tag) 
 
     class Meta:
         database = db
@@ -23,10 +32,30 @@ if not db.table_exists((Secret.__name__).lower()):
 db.close()
 
 
-def _create_secret(name, value):
+def _create_secret(name, value, pass_phrase):
 
     db.connect()
-    secret = Secret.create(name=name, value=value)
+
+    if not pass_phrase:
+        pass_phrase = b"dslp4ssw0rd"  # TODO Replace by random
+
+    else:
+        pass_phrase = pass_phrase.encode()
+
+    encrypted_msg = encrypt_AES_GCM(value, pass_phrase)
+    (kdf_salt, ciphertext, iv, auth_tag) = encrypted_msg
+
+    secret = Secret.create(
+        name=name,
+        kdf_salt=kdf_salt,
+        ciphertext=ciphertext,
+        iv=iv,
+        auth_tag=auth_tag,
+        pass_phrase=pass_phrase,
+    )
+    import pdb
+    pdb.set_trace()
+
     secret.save()
     db.close()
 
@@ -53,12 +82,29 @@ def get_secret(name):
     return secret
 
 
-def _update_secret(name, value):
+def _update_secret(name, value, pass_phrase):
 
     db.connect()
     secret = get_secret(name)
-    secret.value = value
-    secret.save()
+
+    if not pass_phrase:
+        pass_phrase = secret.pass_phrase
+
+    else:
+        pass_phrase = pass_phrase.encode()
+
+    encrypted_msg = encrypt_AES_GCM(value, pass_phrase)
+    (kdf_salt, ciphertext, iv, auth_tag) = encrypted_msg
+
+    query = Secret.update(
+        kdf_salt=kdf_salt,
+        ciphertext=ciphertext,
+        iv=iv,
+        auth_tag=auth_tag,
+        pass_phrase=pass_phrase,
+    ).where(Secret.name == name)
+
+    query.execute()
     db.close()
 
 
@@ -71,3 +117,23 @@ def list_secrets():
 
     db.close()
     return secrets
+
+
+def _find_secret(name, pass_phrase=None):
+
+    db.connect()
+    secret = get_secret(name)
+
+    if not pass_phrase:
+        pass_phrase = secret.pass_phrase  # TODO Replace by random
+
+    else:
+        pass_phrase = pass_phrase.encode()
+
+    enc_msg = secret.generate_enc_msg()
+    secret_val = decrypt_AES_GCM(enc_msg, pass_phrase)
+
+    secret_val = secret_val.decode("utf8")
+    db.close()
+
+    return secret_val
