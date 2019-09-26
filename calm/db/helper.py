5c1f0783cb1d69d30629eda14c7e2bf03aba2e52
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-09-26 18:00:33 +0530

    Some module level changes on crypto library and db library

--- calm/db/helper.py
+++ calm/db/helper.py
@@ -10,18 +10,16 @@ import os
 import datetime
 import uuid
 
-from .crypto import encrypt_AES_GCM, decrypt_AES_GCM
+from .crypto import Crypto
 
 db_location = "calm/db/dsl.db"
 db_location = os.path.abspath(db_location)
-
-db = SqliteDatabase(db_location)
-db.connect()
+dsl_db = SqliteDatabase(db_location)
 
 
 class BaseModel(Model):
     class Meta:
-        database = db
+        database = dsl_db
 
 
 class SecretTable(BaseModel):
@@ -51,137 +49,158 @@ class DataTable(BaseModel):
         return (self.kdf_salt, self.ciphertext, self.iv, self.auth_tag)
 
 
-if not db.table_exists((SecretTable.__name__).lower()):
-    db.create_tables([SecretTable])
+class Secret:
 
-if not db.table_exists((DataTable.__name__).lower()):
-    db.create_tables([DataTable])
+    db = dsl_db
+    secret_table = SecretTable
+    data_table = DataTable
 
-db.close()
+    @classmethod
+    def connect(cls):
+        """
+           Establishes the connection for db access
+           Creates the table in db if not exists
+        """
 
+        if cls.db.is_closed():
+            cls.db.connect()
 
-def _create_secret(name, value, pass_phrase):
-    """Stores the secret in db"""
+        if not cls.db.table_exists((cls.secret_table.__name__).lower()):
+            cls.db.create_tables([cls.secret_table])
 
-    db.connect()
+        if not cls.db.table_exists((cls.secret_table.__name__).lower()):
+            cls.db.create_tables([cls.secret_table])
 
-    if not pass_phrase:
-        pass_phrase = b"dslp4ssw0rd"  # TODO Replace by random
+    @classmethod
+    def close(cls):
+        """Closes the connection"""
 
-    else:
-        pass_phrase = pass_phrase.encode()
+        if not cls.db.is_closed():
+            cls.db.close()
 
-    encrypted_msg = encrypt_AES_GCM(value, pass_phrase)
-    (kdf_salt, ciphertext, iv, auth_tag) = encrypted_msg
+    @classmethod
+    def create(cls, name, value, pass_phrase):
+        """Stores the secret in db"""
 
-    secret = SecretTable.create(name=name, uuid=str(uuid.uuid4()))
+        cls.connect()
 
-    DataTable.create(
-        secret_ref=secret,
-        kdf_salt=kdf_salt,
-        ciphertext=ciphertext,
-        iv=iv,
-        auth_tag=auth_tag,
-        pass_phrase=pass_phrase,
-    )
+        if not pass_phrase:
+            pass_phrase = b"dslp4ssw0rd"  # TODO Replace by random
 
-    db.close()
+        else:
+            pass_phrase = pass_phrase.encode()
 
+        encrypted_msg = Crypto.encrypt_AES_GCM(value, pass_phrase)
+        (kdf_salt, ciphertext, iv, auth_tag) = encrypted_msg
 
-def _delete_secret(name):
-    """Deletes the secret from db"""
+        secret = cls.secret_table.create(name=name, uuid=str(uuid.uuid4()))
 
-    db.connect()
-    secret = get_secret(name)
-    for secret_data in secret.data:
-        secret_data.delete_instance()  # deleting its data
+        cls.data_table.create(
+            secret_ref=secret,
+            kdf_salt=kdf_salt,
+            ciphertext=ciphertext,
+            iv=iv,
+            auth_tag=auth_tag,
+            pass_phrase=pass_phrase,
+        )
 
-    secret.delete_instance()  # deleting that secret
+        cls.close()
 
-    db.close()
+    @classmethod
+    def get_instance(cls, name):
+        """Return secret instance"""
 
+        was_closed = False
 
-def get_secret(name):
-    """Return secret instance"""
+        # If not closed already, do not close on function exit
+        if cls.db.is_closed():
+            cls.connect()
+            was_closed = True
 
-    was_closed = False
-    if db.is_closed():
-        db.connect()
-        was_closed = True
+        secret = cls.secret_table.get(cls.secret_table.name == name)
 
-    secret = SecretTable.get(SecretTable.name == name)
+        if was_closed:
+            cls.close()
 
-    if was_closed:
-        db.close()
+        return secret
 
-    return secret
+    @classmethod
+    def delete(cls, name):
+        """Deletes the secret from db"""
 
+        cls.connect()
+        secret = cls.get_instance(name)
+        for secret_data in secret.data:
+            secret_data.delete_instance()  # deleting its data
 
-def _update_secret(name, value, pass_phrase):
-    """Updates the secret in Database"""
+        secret.delete_instance()  # deleting that secret
 
-    db.connect()
-    secret = get_secret(name)
-    secret_data = secret.data[0]    # using backref
+        cls.close()
 
-    if not pass_phrase:
-        pass_phrase = secret_data.pass_phrase
-    else:
-        pass_phrase = pass_phrase.encode()
+    @classmethod
+    def update(cls, name, value, pass_phrase):
+        """Updates the secret in Database"""
 
-    encrypted_msg = encrypt_AES_GCM(value, pass_phrase)
-    (kdf_salt, ciphertext, iv, auth_tag) = encrypted_msg
+        cls.connect()
+        secret = cls.get_instance(name)
+        secret_data = secret.data[0]    # using backref
 
-    query = DataTable.update(
-        kdf_salt=kdf_salt,
-        ciphertext=ciphertext,
-        iv=iv,
-        auth_tag=auth_tag,
-        pass_phrase=pass_phrase,
-    ).where(DataTable.secret_ref == secret)
+        if not pass_phrase:
+            pass_phrase = secret_data.pass_phrase
+        else:
+            pass_phrase = pass_phrase.encode()
 
-    query.execute()
+        encrypted_msg = Crypto.encrypt_AES_GCM(value, pass_phrase)
+        (kdf_salt, ciphertext, iv, auth_tag) = encrypted_msg
 
-    query = SecretTable.update(last_update_time=datetime.datetime.now()).where(
-        SecretTable.name == name
-    )
+        query = cls.data_table.update(
+            kdf_salt=kdf_salt,
+            ciphertext=ciphertext,
+            iv=iv,
+            auth_tag=auth_tag,
+            pass_phrase=pass_phrase,
+        ).where(cls.data_table.secret_ref == secret)
 
-    query.execute()
+        query.execute()
 
-    db.close()
+        query = cls.secret_table.update(last_update_time=datetime.datetime.now()).where(
+            cls.secret_table.name == name
+        )
 
+        query.execute()
 
-def list_secrets():
-    """returns the Secret object"""
+        cls.close()
 
-    db.connect()
-    secret_basic_configs = []
+    @classmethod
+    def list(cls):
+        """returns the Secret object"""
 
-    for secret in SecretTable.select():
-        secret_basic_configs.append(secret.get_detail_dict())
+        cls.connect()
+        secret_basic_configs = []
 
-    db.close()
-    return secret_basic_configs
+        for secret in cls.secret_table.select():
+            secret_basic_configs.append(secret.get_detail_dict())
 
+        cls.close()
+        return secret_basic_configs
 
-def _find_secret(name, pass_phrase=None):
-    """Find the value of secret"""
+    @classmethod
+    def find(cls, name, pass_phrase=None):
+        """Find the value of secret"""
 
-    db.connect()
-    secret = get_secret(name)
-    secret_data = secret.data[0]  # using backref
+        cls.connect()
+        secret = cls.get_instance(name)
+        secret_data = secret.data[0]  # using backref
 
-    if not pass_phrase:
-        pass_phrase = secret_data.pass_phrase  # TODO Replace by random
-    else:
-        pass_phrase = pass_phrase.encode()
+        if not pass_phrase:
+            pass_phrase = secret_data.pass_phrase  # TODO Replace by random
+        else:
+            pass_phrase = pass_phrase.encode()
 
-    enc_msg = secret_data.generate_enc_msg()
-    secret_val = decrypt_AES_GCM(enc_msg, pass_phrase)
+        enc_msg = secret_data.generate_enc_msg()
+        secret_val = Crypto.decrypt_AES_GCM(enc_msg, pass_phrase)
+        secret_val = secret_val.decode("utf8")
 
-    secret_val = secret_val.decode("utf8")
+        cls.close()
 
-    db.close()
-
-    return secret_val
-
+        return secret_val
