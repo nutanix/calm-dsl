b9c96166a6452583bda193217808ef0f2b160dd9
kdabi <kritagya577@gmail.com>
2019-11-18 13:56:47 +0530

    allow default_target at runbook level #CALM-14069

--- calm/dsl/builtins/models/runbook_service.py
+++ calm/dsl/builtins/models/runbook_service.py
@@ -8,6 +8,12 @@ class RunbookServiceType(ActionType):
     __schema_name__ = "RunbookService"
     __openapi_type__ = "runbook_service"
 
+    def compile(cls):
+        cdict = super().compile()
+        if (cdict.get("default_target_reference", None) or None) is None:
+            cdict.pop("default_target_reference", None)
+        return cdict
+
 
 class RunbookServiceValidator(PropertyValidator, openapi_type="runbook_service"):
     __default__ = None
