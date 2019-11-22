5ee91c85744fdb38f8c5d1357e5bbd870c5b7027
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2019-11-22 10:39:58 +0530

    Removing attrs from options, if variable type="PREDEFINED"

--- calm/dsl/builtins/models/variable.py
+++ calm/dsl/builtins/models/variable.py
@@ -33,6 +33,13 @@ class VariableType(EntityType):
             del cdict["regex"]
         if not cdict.get("editables", {}):
             del cdict["editables"]
+
+        if cdict.get("options", None):
+            options = cdict["options"]
+            # Only EScript/HTTP request info needed for dynamically fetching options
+            if options["type"] == "PREDEFINED":
+                del options["attrs"]
+
         return cdict
 
 
