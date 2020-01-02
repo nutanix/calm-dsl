56ee8bd944bad7e559acc1eab267d3e83a873a62
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2020-01-02 13:06:32 +0530

    Modifying resource.read() api helper

--- calm/dsl/api/resource.py
+++ calm/dsl/api/resource.py
@@ -16,10 +16,11 @@ class ResourceAPI:
             self.PREFIX, verify=False, request_json=payload, method=REQUEST.METHOD.POST
         )
 
-    def read(self, id):
+    def read(self, id=None):
+        url = self.ITEM.format(id) if id else self.PREFIX
         return self.connection._call(
-            self.ITEM.format(id), verify=False, method=REQUEST.METHOD.GET
-        )
+                url, verify=False, method=REQUEST.METHOD.GET
+            )
 
     def update(self, uuid, payload):
         return self.connection._call(
