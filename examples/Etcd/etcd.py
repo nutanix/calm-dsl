2bf20a7496165820e5a32eabfa9abda461306585
kumarsarath588 <kumar.sarath588@gmail.com>
2020-01-30 18:33:36 +0530

    updated etcd bp

--- examples/Etcd/etcd.py
+++ examples/Etcd/etcd.py
@@ -4,14 +4,21 @@ CALM DSL Etcd Blueprint
 """
 
 from calm.dsl.builtins import ref, basic_cred, CalmTask
-from calm.dsl.builtins import action, parallel
+from calm.dsl.builtins import action
 from calm.dsl.builtins import CalmVariable
 from calm.dsl.builtins import Service, Package, Substrate
 from calm.dsl.builtins import Deployment, Profile, Blueprint
-from calm.dsl.builtins import read_provider_spec, read_local_file
+from calm.dsl.builtins import vm_disk_package
+from calm.dsl.builtins import read_local_file
+from calm.dsl.builtins import read_ahv_spec
 
 
-CENTOS_PASSWD = read_local_file("passwd")
+# Path to read ssh private key
+CENTOS_KEY = read_local_file("secrets/private_key")
+# Default credential
+DefaultCred = basic_cred("centos", CENTOS_KEY, name="CENTOS", type="KEY", default=True)
+# Downloadable images for AHV & VMware
+AHV_CENTOS_76 = vm_disk_package(name="AHV_CENTOS_76", config_file="specs/image_config/ahv_centos.yaml")
 
 
 class Etcd(Service):
@@ -47,7 +54,9 @@ class AHV_Etcd(Substrate):
     6 disks (3 X etcd data & 3 X container data)
     """
 
-    provider_spec = read_provider_spec("ahv_spec.yaml")
+    provider_spec = read_ahv_spec(
+        "specs/substrate/ahv_spec_centos.yaml", disk_packages={1: AHV_CENTOS_76}
+    )
 
 
 class EtcdDeployment(Deployment):
@@ -76,18 +85,6 @@ class Nutanix(Profile):
         is_mandatory=True,
         runtime=True,
     )
-    VLAN = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN10 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN11 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN9 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN8 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN7 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN6 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN5 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN4 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN3 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN2 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
-    VLAN1 = CalmVariable.WithOptions.Predefined.string(["DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", "DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_239408"], default="DMSAFSDHFLDSKJHFID-ADFSFDSLKFN_2394087", label="Target Network VLAN", is_mandatory=True, runtime=True)  # noqa
 
     deployments = [EtcdDeployment]
 
@@ -98,8 +95,8 @@ class EtcdDslBlueprint(Blueprint):
     profiles = [Nutanix]
     services = [Etcd]
     substrates = [AHV_Etcd]
-    packages = [EtcdPackage]
-    credentials = [basic_cred("centos", "nutanix/4u", name="CENTOS", default=True)]
+    packages = [EtcdPackage, AHV_CENTOS_76]
+    credentials = [DefaultCred]
 
 
 def main():
