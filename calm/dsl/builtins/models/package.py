049ed61a8242f67fe05e73f82be3a0804b44ecea
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2020-02-10 17:17:19 +0530

    Adding decompile method for packages

--- calm/dsl/builtins/models/package.py
+++ calm/dsl/builtins/models/package.py
@@ -2,8 +2,9 @@ from .entity import EntityType, Entity
 from .validator import PropertyValidator
 
 from .task import dag
-from .action import runbook_create
+from .action import runbook_create, _action_create
 from calm.dsl.tools import get_logging_handle
+from .runbook import RunbookType
 
 
 LOG = get_logging_handle(__name__)
@@ -85,6 +86,42 @@ class PackageType(EntityType):
             raise Exception("Un-supported package type {}".format(ptype))
 
         return cdict
+
+    @classmethod
+    def decompile(mcls, cdict):
+
+        cls = super().decompile(cdict)
+        options = cls.options
+        delattr(cls, "options")
+        
+        option_data = mcls.__validator_dict__["options"][0].decompile(options)
+
+        install_runbook = option_data["install_runbook"]
+        uninstall_runbook = option_data["uninstall_runbook"]
+
+        install_tasks = install_runbook["task_definition_list"]
+        if len(install_tasks) > 1:
+            action_type = "SYSTEM"
+            cls.__install__ = _action_create(
+                **{
+                    "name": "action_install",
+                    "critical": True,
+                    "type": "system",
+                    "runbook": RunbookType.decompile(install_runbook)
+                }
+            )
+
+        uninstall_tasks = uninstall_runbook["task_definition_list"]
+        if len(uninstall_tasks) > 1:
+            cls.__uninstall__ = _action_create(
+                **{
+                    "name": "action_uninstall",
+                    "critical": True,
+                    "type": "system",
+                    "runbook": RunbookType.decompile(uninstall_runbook)
+                }
+            )
+        return cls
 
     def get_task_target(cls):
 
