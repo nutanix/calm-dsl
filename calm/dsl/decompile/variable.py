2122b08c0a8acb22b7b2ed85c1c60f305797f08b
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2020-02-11 19:10:21 +0530

    Adding other type of variable for string data type

--- calm/dsl/decompile/variable.py
+++ calm/dsl/decompile/variable.py
@@ -1,5 +1,6 @@
 from calm.dsl.decompile.render import render_template
-from calm.dsl.builtins import VariableType, CalmVariable, CalmTask
+from calm.dsl.decompile.task import render_task_template
+from calm.dsl.builtins import VariableType, CalmVariable, CalmTask, TaskType
 
 
 def render_variable_template(cls):
@@ -30,8 +31,6 @@ def render_variable_template(cls):
 
     user_attrs["name"] = cls.__name__
 
-    import pdb; pdb.set_trace()
-
     if var_type == "simple":
         is_secret = True if user_attrs["type"] == "SECRET" else False
 
@@ -42,7 +41,38 @@ def render_variable_template(cls):
         else:
             if var_val_type == "STRING":
                 schema_file = "var_simple_string.py.jinja2"
+    
+    else:
+        data_type = cls.data_type
+        options = cls.options.compile(cls)
+        option_type = options.get("type", "PREDEFINED")
 
+        if option_type == "PREDEFINED":
+            user_attrs["choices"] = options.get("choices", [])
+
+            if data_type == "BASE":
+                if var_val_type == "STRING":
+                    schema_file = "var_with_options_predefined.py.jinja2"
+            
+            else:
+                defaults = cls.value
+                user_attrs["value"] = defaults.split(",")
+                if var_val_type == "STRING":
+                    schema_file = "var_with_options_predefined_array.py.jinja2"
+        
+        else:
+            choices = options.pop("choices", None)
+            task = TaskType.decompile(options)
+            user_attrs["value"] = render_task_template(task)
+
+            if data_type == "BASE":
+                if var_val_type == "STRING":
+                    schema_file = "var_with_options_fromTask.py.jinja2"
+            
+            else:
+                if var_val_type == "STRING":
+                    schema_file = "var_with_options_fromTask_array.py.jinja2"
+                
     text = render_template(schema_file=schema_file, obj=user_attrs)
     return text.strip()
 
@@ -63,26 +93,49 @@ var2 = CalmVariable.Simple.Secret(
     is_hidden=True,
     is_mandatory=True,
 )
-var14 = CalmVariable.WithOptions.Predefined.int(
-    ["0", "1"],
-    label="var14_label",
-    regex="^[0-9]+$",
-    validate_regex=True,
-    runtime=True,
-)
-var20 = CalmVariable.WithOptions.Predefined.Array.int(
-    ["0", "1"],
-    label="var20_label",
-    regex="^[0-9]+$",
-    validate_regex=True,
-    runtime=True,
-)
-var26 = CalmVariable.WithOptions.FromTask.int(
-    CalmTask.Exec.escript(script="print '0'"), label="var26_label", validate_regex=True,
-)
-var32 = CalmVariable.WithOptions.FromTask.Array.int(
-    CalmTask.Exec.escript(script="print '0,1'"),
-)
+var13 = CalmVariable.WithOptions(
+        ["var13_val1", "var13_val2"],
+        default="var13_val1",
+        label="var13_label",
+        regex="^[a-zA-Z0-9_]+$",
+        validate_regex=True,
+        runtime=True,
+    )
+var19 = CalmVariable.WithOptions.Predefined.Array(
+        ["var19_val1", "var19_val2"],
+        defaults=["var19_val1", "var19_val2"],
+        label="var19_label",
+        regex="^[a-zA-Z0-9_]+$",
+        validate_regex=True,
+        runtime=True,
+    )
+var25 = CalmVariable.WithOptions.FromTask(
+        CalmTask.HTTP.get(
+            "https://jsonplaceholder.typicode.com/posts/1",
+            # Headers in HTTP variables are bugged:
+            # https://jira.nutanix.com/browse/CALM-13724
+            # headers={"Content-Type": "application/json"},
+            content_type="application/json",
+            verify=True,
+            status_mapping={200: True},
+            response_paths={"var25": "$.title"},
+        ),
+        label="var25_label",
+    )
+
+var31 = CalmVariable.WithOptions.FromTask.Array(
+        CalmTask.HTTP.get(
+            "https://jsonplaceholder.typicode.com/posts/1",
+            # Headers in HTTP variables are bugged:
+            # https://jira.nutanix.com/browse/CALM-13724
+            # headers={"Content-Type": "application/json"},
+            content_type="application/json",
+            verify=True,
+            status_mapping={200: True},
+            response_paths={"var31": "$.title"},
+        ),
+        label="var31_label",
+    )
 var33 = CalmVariable.Simple("sample")
 
 var1 = CalmVariable.Simple(
@@ -94,4 +147,4 @@ var1 = CalmVariable.Simple(
         is_mandatory=True
     )
 
-print(render_variable_template(var2))
+print(render_variable_template(var31))
