975d41ef146e7a4a4ff38d11ba70a5ad9e5c8900
Suryadhulipudi <suryalakshminarayana.d@gmail.com>
2020-02-11 01:58:03 -0800

    Templates for calm setvariable ssh,powershell,escript tasks

--- calm/dsl/decompile/task.py
+++ calm/dsl/decompile/task.py
@@ -23,6 +23,9 @@ def render_task_template(cls):
     cred = cls.attrs.get("login_credential_local_reference", None)
     if cred:
         user_attrs["cred"] = render_ref_template(cred)
+    variables = cls.attrs.get("eval_variables", None)
+    if variables:
+        user_attrs["variables"] = (variables)
     if cls.type == "EXEC":
         script_type = cls.attrs["script_type"]
         cls.attrs["script"] = cls.attrs["script"].replace("'", r"/'")
@@ -34,6 +37,17 @@ def render_task_template(cls):
 
         elif script_type == "npsscript":
             schema_file = "task_exec_powershell.py.jinja2"
+    elif cls.type == "SET_VARIABLE":
+        script_type = cls.attrs["script_type"]
+        cls.attrs["script"] = cls.attrs["script"].replace("'", r"/'")
+        if script_type == "sh":
+            schema_file = "task_setvariable_ssh.py.jinja2"
+
+        elif script_type == "static":
+            schema_file = "task_setvariable_escript.py.jinja2"
+
+        elif script_type == "npsscript":
+            schema_file = "task_setvariable_powershell.py.jinja2"
 
     text = render_template(schema_file=schema_file, obj=user_attrs)
     return text.strip()
@@ -51,4 +65,6 @@ task2 = CalmTask.Exec.ssh(name="Task2", script="echo @@{foo}@@", cred=ref(Defaul
 task3 = CalmTask.Exec.ssh(name="Task3", script="echo @@{foo}@@", target=ref(SampleService))
 task4 = CalmTask.Exec.ssh(name="Task4", script="echo @@{foo}@@", target=ref(SampleService), cred=ref(DefaultCred))
 task5 = CalmTask.Exec.escript(name="Task5", script="echo @@{foo}@@")
-print(render_task_template(task5))
+task6 = CalmTask.Exec.powershell(name="Task5", script="echo @@{foo}@@", cred=ref(DefaultCred))
+task7 = CalmTask.SetVariable.ssh(name="Task5", script="print 'var1=test", variables=["var1"], target=ref(SampleService), cred=ref(DefaultCred))
+print(render_task_template(task7))
