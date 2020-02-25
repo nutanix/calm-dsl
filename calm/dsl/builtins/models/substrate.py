be533fac94b41c22a91ef9c0abd3a7b48440503d
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2020-02-25 15:11:03 +0530

    Minor changes in passing of editables dict

--- calm/dsl/builtins/models/substrate.py
+++ calm/dsl/builtins/models/substrate.py
@@ -148,34 +148,10 @@ class SubstrateType(EntityType):
 
         cdict["readiness_probe"] = readiness_probe
 
-        editables = {}
-        if "editables" in cdict and cdict["editables"]:
-            editables = cdict["editables"]
-        if cdict["type"] == "AHV_VM":
-            if not editables:
-                editables = {
-                    "create_spec": {
-                        "resources": {
-                            "nic_list": {}, 
-                            "serial_port_list": {}
-                        }
-                    }
-                }
-        elif cdict["type"] == "VMWARE_VM":
-            if not editables:
-                editables = {
-                    "create_spec": {
-                        "resources": {
-                            "nic_list": {},
-                            "controller_list": {},
-                            "template_nic_list": {},
-                            "template_controller_list": {},
-                            "template_disk_list": {}
-                        }
-                    }
-                }
-
-        cdict["editables"] = editables
+        editables = cdict.pop("editables", {})
+        cdict["editables"] = {
+            "create_spec" : editables
+        }
 
         return cdict
 
