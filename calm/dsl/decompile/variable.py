b89c3b8fd41b7f2b6968269e5ac0c8bc739aa338
Suryadhulipudi <suryalakshminarayana.d@gmail.com>
2020-03-20 00:30:47 -0700

    Map schema files to templates

--- calm/dsl/decompile/variable.py
+++ calm/dsl/decompile/variable.py
@@ -16,11 +16,6 @@ def render_variable_template(cls):
     if not cls.options:
         var_type = "simple"
 
-    else:
-        options = cls.options.get_dict()
-        if not options.get("choices"):
-            var_type = "simple"
-
     if cls.regex:
         regex = cls.regex.get_dict()
         user_attrs["regex"] = regex.get("value", None)
@@ -43,10 +38,33 @@ def render_variable_template(cls):
         if is_secret:
             if var_val_type == "STRING":
                 schema_file = "var_simple_secret_string.py.jinja2"
+            elif var_val_type == "INT":
+                schema_file = "var_simple_secret_int.py.jinja2"
+            elif var_val_type == "TIME":
+                schema_file = "var_simple_secret_time.py.jinja2"
+            elif var_val_type == "DATE":
+                schema_file = "var_simple_secret_date.py.jinja2"
+            elif var_val_type == "DATE_TIME":
+                schema_file = "var_simple_secret_datetime.py.jinja2"
+            elif var_val_type == "MULTILINE_STRING":
+                user_attrs["value"] = repr(user_attrs["value"])
+                schema_file = "var_simple_secret_multiline.py.jinja2"
 
         else:
             if var_val_type == "STRING":
                 schema_file = "var_simple_string.py.jinja2"
+            elif var_val_type == "INT":
+                schema_file = "var_simple_int.py.jinja2"
+            elif var_val_type == "TIME":
+                schema_file = "var_simple_time.py.jinja2"
+            elif var_val_type == "DATE":
+                schema_file = "var_simple_date.py.jinja2"
+            elif var_val_type == "DATE_TIME":
+                schema_file = "var_simple_datetime.py.jinja2"
+            elif var_val_type == "MULTILINE_STRING":
+                user_attrs["value"] = repr(user_attrs["value"])
+                schema_file = "var_simple_multiline.py.jinja2"
+          
 
     else:
         data_type = cls.data_type
@@ -58,13 +76,35 @@ def render_variable_template(cls):
 
             if data_type == "BASE":
                 if var_val_type == "STRING":
-                    schema_file = "var_with_options_predefined.py.jinja2"
+                    schema_file = "var_with_options_predefined_string.py.jinja2"
+                elif var_val_type == "INT":
+                    schema_file = "var_with_options_predefined_int.py.jinja2"
+                elif var_val_type == "DATE":
+                    schema_file = "var_with_options_predefined_date.py.jinja2"
+                elif var_val_type == "TIME":
+                    schema_file = "var_with_options_predefined_time.py.jinja2"
+                elif var_val_type == "DATE_TIME":
+                    schema_file = "var_with_options_predefined_datetime.py.jinja2"
+                elif var_val_type == "MULTILINE_STRING":
+                    user_attrs["value"] = repr(user_attrs["value"])
+                    schema_file = "var_with_options_predefined_multiline.py.jinja2"
 
             else:
                 defaults = cls.value
                 user_attrs["value"] = defaults.split(",")
                 if var_val_type == "STRING":
-                    schema_file = "var_with_options_predefined_array.py.jinja2"
+                    schema_file = "var_with_options_predefined_array_string.py.jinja2"
+                elif var_val_type == "INT":
+                    schema_file = "var_with_options_predefined_array_int.py.jinja2"
+                elif var_val_type == "DATE":
+                    schema_file = "var_with_options_predefined_array_date.py.jinja2"
+                elif var_val_type == "TIME":
+                    schema_file = "var_with_options_predefined_array_time.py.jinja2"
+                elif var_val_type == "DATE_TIME":
+                    schema_file = "var_with_options_predefined_array_datetime.py.jinja2"
+                elif var_val_type == "MULTILINE_STRING":
+                    user_attrs["value"] = repr(user_attrs["value"])
+                    schema_file = "var_with_options_predefined_array_multiline.py.jinja2"
 
         else:
             options.pop("choices", None)
@@ -73,11 +113,32 @@ def render_variable_template(cls):
 
             if data_type == "BASE":
                 if var_val_type == "STRING":
-                    schema_file = "var_with_options_fromTask.py.jinja2"
-
+                    schema_file = "var_with_options_fromTask_string.py.jinja2"
+                elif var_val_type == "INT":
+                    schema_file = "var_with_options_fromTask_int.py.jinja2"
+                elif var_val_type == "DATE":
+                    schema_file = "var_with_options_fromTask_date.py.jinja2"
+                elif var_val_type == "TIME":
+                    schema_file = "var_with_options_fromTask_time.py.jinja2"
+                elif var_val_type == "DATE_TIME":
+                    schema_file = "var_with_options_fromTask_datetime.py.jinja2"
+                elif var_val_type == "MULTILINE_STRING":
+                    user_attrs["value"] = repr(user_attrs["value"])
+                    schema_file = "var_with_options_fromTask_multiline.py.jinja2"
             else:
                 if var_val_type == "STRING":
-                    schema_file = "var_with_options_fromTask_array.py.jinja2"
+                    schema_file = "var_with_options_fromTask_array_string.py.jinja2"
+                elif var_val_type == "INT":
+                    schema_file = "var_with_options_fromTask_array_int.py.jinja2"
+                elif var_val_type == "DATE":
+                    schema_file = "var_with_options_fromTask_array_date.py.jinja2"
+                elif var_val_type == "TIME":
+                    schema_file = "var_with_options_fromTask_array_time.py.jinja2"
+                elif var_val_type == "DATE_TIME":
+                    schema_file = "var_with_options_fromTask_array_datetime.py.jinja2"
+                elif var_val_type == "MULTILINE_STRING":
+                    user_attrs["value"] = repr(user_attrs["value"])
+                    schema_file = "var_with_options_fromTask_array_multiline.py.jinja2"
 
     if not schema_file:
         raise Exception("Unknown variable type")
