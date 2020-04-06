5d860b0646b4b82d6943b3703f3fcf780cff9440
Balaji <balaji@nutanix.com>
2020-04-06 13:06:04 -0700

    That which we call rose by any other name would smell as sweet
    But,
     - Remove use of "CALM" in messages. Use "Calm" instead.
    
    Also,
     - Fix flake error

--- calm/dsl/api/version.py
+++ calm/dsl/api/version.py
@@ -5,15 +5,15 @@ class VersionAPI:
     def __init__(self, connection):
         self.connection = connection
 
-        self.CALM_VERSION = "apps/version"
-        self.PC_VERSION = "PrismGateway/services/rest/v1/cluster/version"
+        self.calm_version = "apps/version"
+        self.pc_version = "PrismGateway/services/rest/v1/cluster/version"
 
     def get_calm_version(self):
         return self.connection._call(
-            self.CALM_VERSION, verify=False, method=REQUEST.METHOD.GET,
+            self.calm_version, verify=False, method=REQUEST.METHOD.GET,
         )
 
     def get_pc_version(self):
         return self.connection._call(
-            self.PC_VERSION, verify=False, method=REQUEST.METHOD.GET,
+            self.pc_version, verify=False, method=REQUEST.METHOD.GET,
         )
