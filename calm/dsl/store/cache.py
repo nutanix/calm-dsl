56625bb4a02f9a3fb67c6619c6360c96111e1454
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2020-05-06 12:01:05 +0530

    Updating cache handle

--- calm/dsl/store/cache.py
+++ calm/dsl/store/cache.py
@@ -1,5 +1,6 @@
 import peewee
 import click
+import sys
 
 from ..db import get_db_handle
 from calm.dsl.api import get_resource_api, get_api_client
@@ -11,79 +12,74 @@ LOG = get_logging_handle(__name__)
 class Cache:
     """Cache class Implementation"""
 
-    cache_types = ["ahv_subnet", "ahv_disk_image"]
-
     @classmethod
-    def get_entity_types(cls):
-        """Entity types used in the cache"""
-        return list(cls.entity_type_api_map.keys())
-
-    @classmethod
-    def create(*args, **kwargs):
+    def get_cache_tables(cls):
+        """returns tables used for cache purpose"""
 
         db = get_db_handle()
+        db_tables = db.registered_tables
+
+        cache_tables = {}
+        for table in db_tables:
+            if hasattr(table, "__cache_type__"):
+                cache_tables[table.__cache_type__] = table
+        return cache_tables
+
+    @classmethod
+    def create(cls, *args, **kwargs):
+        """creates a entry in cache table"""
+
+        cache_tables = cls.get_cache_tables()
         entity_type = kwargs.pop("entity_type", None)
         if not entity_type:
-            raise Exception("No entity type supplied")
+            LOG.error("No entity type for cache supplied")
+            sys.exit(-1)
 
-        # TODO validate by proper cache table
-        if hasattr(db, entity_type):
-            db_cls = getattr(db, entity_type)
-
-        else:
-            raise Exception("Unknown entity type")
+        db_cls = cache_tables.get(entity_type, None)
+        if not db_cls:
+            LOG.error("Unknown entity type ({}) supplied".format(entity_type))
+            sys.exit(-1)
 
         db_cls.create(**kwargs)
 
     @classmethod
-    def get_entity_uuid(*args, **kwargs):
+    def get_entity_uuid(cls, *args, **kwargs):
+        """returns entity uuid corresponding to supplied entry"""
 
-        db = get_db_handle()
+        cache_tables = cls.get_cache_tables()
         entity_type = kwargs.pop("entity_type", None)
         if not entity_type:
-            raise Exception("No entity type supplied")
+            LOG.error("No entity type for cache supplied")
+            sys.exit(-1)
 
-        # TODO validate by proper cache table
-        if hasattr(db, entity_type):
-            db_cls = getattr(db, entity_type)
-
-        else:
-            raise Exception("Unknown entity type")
+        db_cls = cache_tables.get(entity_type, None)
+        if not db_cls:
+            LOG.error("Unknown entity type ({}) supplied".format(entity_type))
+            sys.exit(-1)
 
         return db_cls.get_entity_uuid(**kwargs)
 
     @classmethod
     def sync(cls):
-        """Update the cache by latest data"""
+        """Sync cache by latest data"""
 
-        db = get_db_handle()
-        db_tables = db.registered_tables
-
-        # Find cache tables and sync data
-        for table in db_tables:
-            if hasattr(table, "__cache_type__"):
-                table.sync()
+        cache_tables = cls.get_cache_tables()
+        for table in list(cache_tables.values()):
+            table.sync()
 
     @classmethod
     def clear_entities(cls):
-        """Deletes all the data present in the cache"""
+        """Clear data present in the cache tables"""
 
-        db = get_db_handle()
-        db_tables = db.registered_tables
-
-        # Find cache tables and clear data
-        for table in db_tables:
-            if hasattr(table, "__cache_type__"):
-                table.clear()
+        cache_tables = cls.get_cache_tables()
+        for table in list(cache_tables.values()):
+            table.clear()
 
     @classmethod
     def show_data(cls):
-        db = get_db_handle()
-        db_tables = db.registered_tables
+        """Display data present in cache tables"""
 
-        # Find cache tables and clear data
-        for table in db_tables:
-            if hasattr(table, "__cache_type__"):
-                cache_type = table.__cache_type__
-                click.echo("\n{}".format(cache_type.upper()))
-                table.show_data()
+        cache_tables = cls.get_cache_tables()
+        for cache_type, table in cache_tables.items():
+            click.echo("\n{}".format(cache_type.upper()))
+            table.show_data()
