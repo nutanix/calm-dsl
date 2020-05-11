21bf3458b1c6425ad0d253de1a299886efa0f5b1
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2020-05-11 18:03:31 +0530

    Minor fix and test improvements

--- tests/cli/test_app_commands.py
+++ tests/cli/test_app_commands.py
@@ -1,5 +1,7 @@
 import pytest
 import time
+import json
+import traceback
 from click.testing import CliRunner
 
 from calm.dsl.cli import main as cli
@@ -27,22 +29,54 @@ class TestAppCommands:
     def test_apps_list(self):
         runner = CliRunner()
         result = runner.invoke(cli, ["get", "apps"])
-        assert result.exit_code == 0
         if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
             pytest.fail("BP Get failed")
-        print(result.output)
+        LOG.info("Success")
 
     def test_apps_list_with_limit_offset(self):
         runner = CliRunner()
         result = runner.invoke(cli, ["get", "apps", "--limit=15", "--offset=5"])
-        assert result.exit_code == 0
-        print(result.output)
+        if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
+        LOG.info("Success")
 
     def test_apps_list_with_name(self):
         runner = CliRunner()
         result = runner.invoke(cli, ["get", "apps", "--name=MSSQL"])
-        assert result.exit_code == 0
-        print(result.output)
+        if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
+        LOG.info("Success")
 
     def _create_bp(self):
         runner = CliRunner()
@@ -58,7 +92,18 @@ class TestAppCommands:
                 "--description='Test DSL Blueprint; to delete'",
             ],
         )
-        assert result.exit_code == 0
+        if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
         LOG.info("Success")
 
     def test_app_create_describe_actions(self):
@@ -76,7 +121,18 @@ class TestAppCommands:
             ],
             input="\n".join(["1", "DEV"]),
         )
-        assert result.exit_code == 0
+        if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
         LOG.info("Success")
 
         self._test_describe_app()
@@ -99,7 +155,18 @@ class TestAppCommands:
                 "--launch_params={}".format(DSL_LAUNCH_PARAM_FILEPATH),
             ],
         )
-        assert result.exit_code == 0
+        if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
         LOG.info("Success")
 
         self._test_describe_app()
@@ -111,9 +178,19 @@ class TestAppCommands:
         runner = CliRunner()
         LOG.info("Running 'calm describe app' command")
         result = runner.invoke(cli, ["describe", "app", self.created_app_name])
-        assert result.exit_code == 0
+        if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
         LOG.info("Success")
-        LOG.debug("Command output : {}".format(result.output))
 
     def _wait_for_non_busy_state(self):
         runner = CliRunner()
@@ -141,26 +218,56 @@ class TestAppCommands:
                 "--app={}".format(self.created_app_name),
             ],
         )
-        assert result.exit_code == 0
+        if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
         LOG.info("Success")
-        LOG.debug("Command output : {}".format(result.output))
 
     def _test_app_delete(self):
         runner = CliRunner()
         self._wait_for_non_busy_state()
         LOG.info("Deleting App {} ".format(self.created_app_name))
         result = runner.invoke(cli, ["delete", "app", self.created_app_name])
-        assert result.exit_code == 0
+        if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
         LOG.info("Success")
-        LOG.debug("Response : {}".format(result.output))
 
     def _test_dsl_bp_delete(self):
         runner = CliRunner()
         LOG.info("Deleting Bp {} ".format(self.created_dsl_bp_name))
         result = runner.invoke(cli, ["delete", "bp", self.created_dsl_bp_name])
-        assert result.exit_code == 0
+        if result.exit_code:
+            cli_res_dict = {"Output": result.output, "Exception": str(result.exception)}
+            LOG.debug(
+                "Cli Response: {}".format(
+                    json.dumps(cli_res_dict, indent=4, separators=(",", ": "))
+                )
+            )
+            LOG.debug(
+                "Traceback: \n{}".format(
+                    "".join(traceback.format_tb(result.exc_info[2]))
+                )
+            )
         LOG.info("Success")
-        LOG.debug("Response : {}".format(result.output))
 
 
 if __name__ == "__main__":
