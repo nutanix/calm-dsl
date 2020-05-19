f22aecd6411d234646e8f20575135764721cc960
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2020-05-19 18:05:24 +0530

    Changes in project api handler

--- calm/dsl/api/project.py
+++ calm/dsl/api/project.py
@@ -7,6 +7,10 @@ class ProjectAPI(ResourceAPI):
         super().__init__(connection, resource_type="projects")
         self.CREATE_PREFIX = ResourceAPI.ROOT + "/projects_internal"
         self.INTERNAL_ITEM = self.CREATE_PREFIX + "/{}"
+        self.CALM_PROJECTS_PREFIX = ResourceAPI.ROOT + "/calm_projects"
+        self.CALM_PROJECTS_PREFIX_ITEM = self.CALM_PROJECTS_PREFIX + "/{}"
+        self.TASK_POLL = self.CALM_PROJECTS_PREFIX_ITEM + "/pending_tasks/{}"
+        self.USAGE = self.CALM_PROJECTS_PREFIX_ITEM + "/usage"
 
     def create(self, payload):
         return self.connection._call(
@@ -18,7 +22,7 @@ class ProjectAPI(ResourceAPI):
 
     def update(self, uuid, payload):
         return self.connection._call(
-            self.INTERNAL_ITEM.format(uuid),
+            self.CALM_PROJECTS_PREFIX_ITEM.format(uuid),
             verify=False,
             request_json=payload,
             method=REQUEST.METHOD.PUT,
@@ -27,4 +31,21 @@ class ProjectAPI(ResourceAPI):
     def read(self, id):
         return self.connection._call(
             self.INTERNAL_ITEM.format(id), verify=False, method=REQUEST.METHOD.GET
+        )
+
+    def usage(self, uuid):
+        return self.connection._call(
+            self.USAGE.format(id), verify=False, method=REQUEST.METHOD.POST
+        )
+    
+    def delete(self, uuid):
+        return self.connection._call(
+            self.CALM_PROJECTS_PREFIX_ITEM.format(uuid), verify=False, method=REQUEST.METHOD.DELETE
+        )
+    
+    def poll_task(self, project_uuid, task_uuid):
+        return self.connection._call(
+            self.TASK_POLL.format(project_uuid, task_uuid),
+            verify=False,
+            method=REQUEST.METHOD.GET,
         )
