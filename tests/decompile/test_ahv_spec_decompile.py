b288b5e7a2c4d49bd63d7393f9fa91cb28a641a2
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2020-06-19 19:30:10 +0530

    Adding templates and render helper for ahv vm nics

--- tests/decompile/test_ahv_spec_decompile.py
+++ tests/decompile/test_ahv_spec_decompile.py
@@ -1,6 +1,7 @@
 from calm.dsl.builtins import AhvVmType, AhvVmResourcesType
 from calm.dsl.builtins import read_spec
 from calm.dsl.decompile.ahv_vm_disk import render_ahv_vm_disk
+from calm.dsl.decompile.ahv_vm_nic import render_ahv_vm_nic
 
 
 def test_decompile():
@@ -13,6 +14,9 @@ def test_decompile():
     # Get rendered disks
     for disk in vm_resources.disks:
         print(render_ahv_vm_disk(disk))
+    
+    for nic in vm_resources.nics:
+        print(render_ahv_vm_nic(nic))
 
     import pdb
 
