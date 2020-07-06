26cfad4fa1bbf3875f343e75e0a681093069f629
kdabi <kritagya.dabi@nutanix.com>
2020-07-06 22:22:52 +0530

    added json tests for runbooks

---
+++ tests/sample_runbooks/test_runbook_json.py
@@ -0,0 +1,40 @@
+"""
+Test for testing runbook generated json against known json
+"""
+import os
+from calm.dsl.runbooks import runbook_json
+
+from existing_endpoint import DslExistingEndpoint
+from parallel import DslParallelRunbook
+from runbook_variables import DslRunbookWithVariables
+from simple_runbook import DslSimpleRunbook
+
+
+def test_runbook_json(Runbook, json_file):
+    """
+    Test the generated json for a runbook agains known output
+    """
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(dir_path, json_file)
+
+    generated_json = runbook_json(Runbook)
+    known_json = open(file_path).read()
+
+    assert generated_json == known_json
+
+
+def test_runbooks():
+    runbooks = {
+        "test_existing_endpoint.json": DslExistingEndpoint,
+        "test_parallel.json": DslParallelRunbook,
+        "test_runbook_variables.json": DslRunbookWithVariables,
+        "test_simple_runbook.json": DslSimpleRunbook,
+    }
+
+    for json_file, runbook in runbooks.items():
+        test_runbook_json(runbook, json_file)
+
+
+if __name__ == "__main__":
+    test_runbooks()
