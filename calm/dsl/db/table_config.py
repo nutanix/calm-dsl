34ba5a523510edefe1b587b6b6483aef7658bc89
kdabi <kritagya.dabi@nutanix.com>
2020-07-09 10:04:57 +0530

    added a check for account_type

--- calm/dsl/db/table_config.py
+++ calm/dsl/db/table_config.py
@@ -470,7 +470,10 @@ class ProjectCache(CacheTableBase):
             account_map = {}
             for account in account_list:
                 account_uuid = account["uuid"]
-                account_type = account_uuid_type_map[account_uuid]
+                if account_uuid in account_uuid_type_map:
+                    account_type = account_uuid_type_map[account_uuid]
+                else:
+                    continue
 
                 # For now only single provider account per provider is allowed
                 account_map[account_type] = account_uuid
