7c0f216dbe48e4a48b40a3afc8c778c19416e5c1
Balaji <balaji@nutanix.com>
2020-07-23 19:11:50 -0700

    format using black

--- calm/dsl/cli/directory_service_commands.py
+++ calm/dsl/cli/directory_service_commands.py
@@ -6,15 +6,27 @@ from .main import get
 
 
 @get.command("directory_services")
-@click.option("--name", "-n", default=None, help="Search for directory services by name")
 @click.option(
-    "--filter", "filter_by", "-f", default=None, help="Filter directory services by this string"
+    "--name", "-n", default=None, help="Search for directory services by name"
+)
+@click.option(
+    "--filter",
+    "filter_by",
+    "-f",
+    default=None,
+    help="Filter directory services by this string",
 )
 @click.option("--limit", "-l", default=20, help="Number of results to return")
 @click.option(
     "--offset", "-s", default=0, help="Offset results by the specified amount"
 )
-@click.option("--quiet", "-q", is_flag=True, default=False, help="Show only directory service names")
+@click.option(
+    "--quiet",
+    "-q",
+    is_flag=True,
+    default=False,
+    help="Show only directory service names",
+)
 @click.option(
     "--out",
     "-o",
