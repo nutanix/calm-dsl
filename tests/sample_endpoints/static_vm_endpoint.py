9cb577bbbf40ce347183800b58d192fc0667d563
kdabi <kritagya.dabi@nutanix.com>
2020-09-10 12:38:09 +0530

    fixed vm endpoint model with vm_references

--- tests/sample_endpoints/static_vm_endpoint.py
+++ tests/sample_endpoints/static_vm_endpoint.py
@@ -5,7 +5,7 @@ Calm VM Endpoint Example with Static Filter
 from calm.dsl.runbooks import read_local_file
 from calm.dsl.runbooks import basic_cred
 from calm.dsl.runbooks import CalmEndpoint as Endpoint
-from calm.dsl.runbooks import CalmAccount as Account
+from calm.dsl.runbooks import CalmAccount as Account, VM
 from calm.dsl.runbooks import ENDPOINT_FILTER, ENDPOINT_PROVIDER
 
 CRED_USERNAME = read_local_file(".tests/runbook_tests/username")
@@ -16,7 +16,7 @@ LinuxCred = basic_cred(CRED_USERNAME, CRED_PASSWORD, name="endpoint_cred")
 
 AHVStaticVMEndpoint = Endpoint.Linux.vm(
     filter_type=ENDPOINT_FILTER.STATIC,
-    values=[AHV_LINUX_ID],
+    vms=[VM(uuid=AHV_LINUX_ID, name="ahv_vm")],
     cred=LinuxCred,
     provider_type=ENDPOINT_PROVIDER.NUTANIX,
     account=Account.NutanixPC("NTNX_LOCAL_AZ"),
