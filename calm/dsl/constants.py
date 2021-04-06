92ce53c30d7ab20df2c7f94c51198e2135fd5e87
abhijeetkaurav1st <abhijeet.kaurav@nutanix.com>
2021-04-06 14:04:26 +0530

    Project task changes for better logs.

--- calm/dsl/constants.py
+++ calm/dsl/constants.py
@@ -26,3 +26,26 @@ PROVIDER_ACCOUNT_TYPE_MAP = {
     "AZURE_VM": "azure",
     "GCP_VM": "gcp",
 }
+
+
+class PROJECT_TASK:
+    class STATUS:
+        PENDING = "pending"
+        RUNNING = "running"
+        ABORTED = "aborted"
+        SUCCESS = "success"
+        SUSPENDED = "waiting"
+        FAILURE = "failure"
+
+    TERMINAL_STATES = [
+        STATUS.ABORTED,
+        STATUS.SUCCESS,
+        STATUS.FAILURE,
+    ]
+
+    NON_TERMINAL_STATES = [
+        STATUS.RUNNING,
+        STATUS.PENDING,
+    ]
+
+    FAILURE_STATES = [STATUS.ABORTED, STATUS.SUSPENDED, STATUS.FAILURE]
