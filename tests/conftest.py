fe6434da7a521ce8d410e97122d9fa06f35891bb
Abhijeet Singh Kaurav <abhijeet.kaurav@nutanix.com>
2022-02-11 15:12:14 +0530

    Revert "Delete dsl setup created vms post whole test suit has finished (#113)" (#121)
    This reverts commit 4a5025538d9b1150b525fcfb39d27b9659d5b139.
    
    (cherry picked from commit 00b31f6eaeee2374185d5ab6f17d6e164fff0894)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,11 @@
-import json
-import os
 import sys
 import pytest
 import logging
 from pytest_reportportal import RPLogger, RPLogHandler
 
-
-from calm.dsl.api.handle import get_api_client
-from calm.dsl.api.resource import get_resource_api
 from calm.dsl.log import CustomLogging, get_logging_handle
 
 LOG = get_logging_handle(__name__)
-HOME = os.path.expanduser("~")
-DSL_TEST_FILES_PREFIX = HOME + "/.calm/.local/.tests/runbook_tests/"
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -44,51 +37,3 @@ def rp_logger(request):
 
     rp_handler.setLevel(logging.DEBUG)
     return logger
-
-
-def pytest_sessionfinish(session, exitstatus):
-    """
-    This routine is called after whole test run finished,
-    right before returning the exit status to the system.
-    Args:
-        session (pytest.Session) – The pytest session object.
-        exitstatus (int) – The status which pytest will return to the system
-    """
-    LOG.debug("All tests finished. Deleting VMs created by test setup of dsl.")
-    vm_uuid_files = [
-        "ahv_linux_id",
-        "ahv_linux_id_with_no_ip",
-        "ahv_linux_id_with_powered_off",
-        "vm_actions_ahv_on",
-        "vm_actions_ahv_off",
-    ]
-
-    # Read uuid of vms from files and delete them
-    try:
-        api_client = get_api_client()
-        vm_obj = get_resource_api("vms", api_client.connection)
-        for file_name in vm_uuid_files:
-            vm_uuid = None
-            file_name = DSL_TEST_FILES_PREFIX + file_name
-            if os.path.exists(file_name):
-                with open(file_name, "r") as fp:
-                    vm_uuid = fp.read()
-
-                try:
-                    LOG.debug("Deleting vm with uuid {} .".format(vm_uuid))
-                    res, err = vm_obj.delete(vm_uuid)
-                    LOG.debug(
-                        "Response for delete vm:{}".format(json.dumps(res.json()))
-                    )
-                    if err:
-                        raise Exception(err)
-                except Exception as ex:
-                    LOG.debug(
-                        "Delete of vm with uuid {} failed with error: {}. Try deleting manually.".format(
-                            vm_uuid, ex
-                        )
-                    )
-    except Exception as ex:
-        LOG.debug(
-            "Delete of vms failed with error: {}. Try deleting manually.".format(ex)
-        )
