51ab0c5db3d97741319525648b12e6456e118f8d
rajpanchal00 <raj.panchal@nutanix.com>
2022-09-01 11:16:38 +0530

    Fix users list route (#161)
    * Fix users list route
    
    * Make black
    
    (cherry picked from commit 61515562636fa4aefc7f0d71ef84d38a40c1560a)

--- calm/dsl/api/user.py
+++ calm/dsl/api/user.py
@@ -1,6 +1,19 @@
 from .resource import ResourceAPI
+from .connection import REQUEST
 
 
 class UserAPI(ResourceAPI):
     def __init__(self, connection):
         super().__init__(connection, resource_type="users")
+
+    # Temporary hack to fix blocker CALM-32740
+    def list(self, params={}, ignore_error=False):
+        params.pop("sort_attribute", None)
+        params.pop("sort_order", None)
+        return self.connection._call(
+            self.LIST,
+            verify=False,
+            request_json=params,
+            method=REQUEST.METHOD.POST,
+            ignore_error=ignore_error,
+        )
