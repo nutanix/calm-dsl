4745c849ba7a152f1f5dd2e2813f70c291995c5e
Nisarg <nisarg.shah1@nutanix.com>
2022-09-12 15:08:48 +0530

    Increasing timeout for marketplace API (#170)
    * Update marketplace.py
    
    * Make black changes
    
    (cherry picked from commit e42d39c61bcfe062f3ef4a2b82b6e6fdc114ce36)

--- calm/dsl/api/marketplace.py
+++ calm/dsl/api/marketplace.py
@@ -7,8 +7,14 @@ class MarketPlaceAPI(ResourceAPI):
         super().__init__(connection, resource_type="calm_marketplace_items")
         self.VARIABLE_VALUES = self.ITEM + "/variables/{}/values"
 
+    # https://jira.nutanix.com/browse/CALM-33232
+    # Marketplace API taking more than 30s so setting it to 300s
     def variable_values(self, uuid, var_uuid, payload={}):
         url = self.VARIABLE_VALUES.format(uuid, var_uuid)
         return self.connection._call(
-            url, verify=False, method=REQUEST.METHOD.POST, request_json=payload
+            url,
+            verify=False,
+            method=REQUEST.METHOD.POST,
+            request_json=payload,
+            timeout=(5, 300),
         )
