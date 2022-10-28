5f7b2d100a386a56cfa0632bb544fab989afc767
fabricekrebs <fabrice.krebs@nutanix.com>
2022-10-28 13:18:43 +0200

    Update network_group_tunnel.py (#249)

--- examples/NetworkGroupTunnel/network_group_tunnel.py
+++ examples/NetworkGroupTunnel/network_group_tunnel.py
@@ -6,5 +6,5 @@ class NewNetworkGroupTunnel2(NetworkGroupTunnel):
     """Network group tunnel for test"""
     account = Ref.Account("NTNX_LOCAL_AZ")
     platform_vpcs = [Ref.Vpc("test_ng_vpc_61ee30a66f0b", account_name="NTNX_LOCAL_AZ")] # Resolve account name from parent.
-    tunnel_vm_spec = ahv_network_group_tunnel_vm_spec("auto_cluster_prod_4fee9603c584", "ng_tunnel_vpc_subnet_e61ad7fdb778")
+    tunnel_vm_spec = ahv_network_group_tunnel_vm_spec("auto_cluster_prod_4fee9603c584", "ng_tunnel_vpc_subnet_e61ad7fdb778") #ahv_network_group_tunnel_vm_spec is requireding as first argument a Nutanix Cluster name, and as second argument a subnet internal to the VPC
 
