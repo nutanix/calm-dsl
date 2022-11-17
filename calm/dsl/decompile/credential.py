20a6de1eaeac4643b87389a980d1d7c5b4aee8c5
Utkarsh Bairolia <utkarsh.bairolia@nutanix.com>
2022-11-17 11:57:31 +0530

    added dynamic cred support during compile (#193)
    (cherry picked from commit 9bf45c9b02f2e53c0fe3f0792f48b043adc197ee)

--- calm/dsl/decompile/credential.py
+++ calm/dsl/decompile/credential.py
@@ -21,26 +21,47 @@ def render_credential_template(cls):
     user_attrs = cls.get_user_attrs()
     user_attrs["description"] = cls.__doc__
 
+    cred_type = user_attrs.get("cred_class", "")
+
     var_name = "BP_CRED_{}".format(get_valid_identifier(cls.__name__))
-    file_name = "{}_{}".format(var_name, user_attrs["type"])
+    user_attrs["var_name"] = var_name
+    if user_attrs.get("editables", {}):
+        user_attrs["editables"] = user_attrs["editables"].get_dict()
+    CRED_VAR_NAME_MAP[user_attrs["name"]] = var_name
+
+    if cred_type == "static":
+
+        file_name = "{}_{}".format(var_name, user_attrs["type"])
+        create_file_from_file_name(file_name)
+        user_attrs["value"] = file_name
+        text = render_template("basic_credential.py.jinja2", obj=user_attrs)
+
+    elif cred_type == "dynamic":
+
+        for var_obj in user_attrs.get("variable_list", []):
+            if var_obj.type == "SECRET":
+                file_name = "{}_VAR_{}_SECRET".format(
+                    var_name, get_valid_identifier(var_obj.name)
+                )
+                create_file_from_file_name(file_name)
+                var_obj.value = file_name
+
+        text = render_template("dynamic_credential.py.jinja2", obj=user_attrs)
+
+    else:
+        raise TypeError("{} is not a supported cred class".format(cred_type))
+    return text.strip()
+
+
+def create_file_from_file_name(file_name):
+    """create a file on local directory and add to global file stack for given file name"""
     file_loc = os.path.join(get_local_dir(), file_name)
 
     # Storing empty value in the file
     with open(file_loc, "w+") as fd:
         fd.write("")
 
-    user_attrs["var_name"] = var_name
-    user_attrs["value"] = file_name
-
-    if user_attrs.get("editables", {}):
-        user_attrs["editables"] = user_attrs["editables"].get_dict()
-
-    # update the map
-    CRED_VAR_NAME_MAP[user_attrs["name"]] = var_name
     CRED_FILES.append(file_name)
-
-    text = render_template("credential.py.jinja2", obj=user_attrs)
-    return text.strip()
 
 
 def get_cred_var_name(cred_name):
