51cd3aa4ecafe5aa68b4b2cd203263316cb36382
Abhijeet Kaurav <abhijeet.kaurav@nutanix.com>
2023-08-06 13:01:23 +0530

    Bump version  to 3.7.0
    (cherry picked from commit f3b933e860f77a9ec9e5acf2261f0b86062fb49d)

--- calm/dsl/cli/main.py
+++ calm/dsl/cli/main.py
@@ -44,7 +44,7 @@ LOG = get_logging_handle(__name__)
     default=False,
     help="Update cache before running command",
 )
-@click.version_option("3.6.1")
+@click.version_option("3.7.0")
 @click.pass_context
 def main(ctx, config_file, sync):
     """Calm CLI
