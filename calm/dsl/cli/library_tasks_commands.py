835b234b7b41f49f6da0a4c23bd80f0eb25a0f88
Hemanthvarkolu21 <hemanth.varkolu@nutanix.com>
2023-10-09 13:31:06 +0530

    import library task cmd file fix (#295)
    (cherry picked from commit 5211ab451afd31ca58b24427c1a10247db55dfc8)

--- calm/dsl/cli/library_tasks_commands.py
+++ calm/dsl/cli/library_tasks_commands.py
@@ -82,7 +82,7 @@ def _delete_task(task_names):
     "task_file",
     type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
     required=True,
-    help="Path of task file (.json, .sh, .escript, .escript.py2, .escript.py3, .ps1)",
+    help="Path of task file (.sh, .escript, .escript.py2, .escript.py3, .ps1)",
 )
 @click.option("--name", "-n", default=None, help="Task Library item name (Optional)")
 @click.option(
