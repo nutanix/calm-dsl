4fa0689627c6b6838678beb3e151133e993fda00
Prabhat Dwivedi <prabhat.dwivedi@nutanix.com>
2026-09-11 14:58:07 +0530

    ENG-980229: Fix old host fetching during initialisation (#860)
    [ENG-980229](https://jira.nutanix.com/browse/ENG-980229)
    
    Description:
    1. `calm init dsl` was breaking in fresh machines. Reason is due to
    missing config.ini.
    2. As new machines don't have config.ini. Hence, fetching pc ip from
    config breaks.
    3. Switching to old code where we fetch pc ip in fresh setups using
    version cache.
    
    Test Plan:
    1. Integrate cleanup stage in all pipeline where dsl test runs.
    2. Cleanup stage should delete ~/.calm folder which contains config.ini
    . This will help slave machines act as fresh installation for DSL and
    catch these kind of errors in future. PR:
    https://github.com/ideadevice/jenkins-pipeline-library/pull/672
    
    (cherry picked from commit 318c02ac54764dc425659449f60f157cb3d7a95b)

--- calm/dsl/cli/main.py
+++ calm/dsl/cli/main.py
@@ -92,8 +92,7 @@ def main(ctx, config_file, sync):
             old_host = nc_server_config.get("host", "")
             LOG.debug("Old NC host: {}".format(old_host))
         else:
-            server_config = ContextObj.get_server_config()
-            old_host = server_config.get("pc_ip", "")
+            old_host = Version.get_version_data("PC").get("pc_ip", "")
 
         if config_file:
             if not os.path.exists(config_file):
